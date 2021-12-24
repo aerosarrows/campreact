f9feff7d9fcd03b79327fedf49d0d3a609413260
Geoffrey Lisch <>
2021-12-24 10:33:04 -0700

    Components lesson

--- src/components/CampsiteInfoComponents.js
+++ src/components/CampsiteInfoComponents.js
@@ -8,6 +8,27 @@ class CampsiteInfo extends Component {
             campsite : null
         };
     }
+
+    renderComments(comments) {
+        if (comments) {
+            return (
+                <div className = 'col-md-5 m-1'>
+                    <h4>Comments</h4>
+                    {comments.map(comment => {
+                        return (
+                            <div>
+                                <p>{comment.text}</p>
+                                <p>{comment.author} {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
+                            </div>
+                        )
+                    })}
+            </div>
+            );
+        }
+        return <div>
+        </div>
+    }
+
     renderCampsite(campsite) {
         return (
             <div className='col-md-5 m-1'>
@@ -22,12 +43,16 @@ class CampsiteInfo extends Component {
         )
     }
 
+    
 
-    render () {
+    render (props) {
         if (this.props.campsite){
             return (
-                <div className='row'>
-                    {this.renderCampsite(this.props.campsite)}
+                <div className='container'>
+                    <div className='row'>
+                        {this.renderCampsite(this.props.campsite)}
+                        {this.renderComments(this.props.campsite.comments)}
+                    </div>
                 </div>
             );
         }
